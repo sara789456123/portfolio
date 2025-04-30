b0f4a1c610e5f3d243e2103150e3c3416a191283
Sara <sara.grs09@gmail.com>
2025-04-30 09:44:00 +0200

    ef

--- clickerCapi/save_score.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $score = $_POST['score'];
-    file_put_contents('score.txt', $score);
-    echo 'Score sauvegarder!';
-} else {
-    echo 'Invalid request method.';
-}
-?>
